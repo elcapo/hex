deff3276d2c0098761b1db38cba108c04d7e4c3e
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-03-20 20:28:14 +0000

    use 4 spaces for indentation

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -4,6 +4,6 @@
 #include "board_test.cpp"
 
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
