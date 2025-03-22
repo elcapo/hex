547be3d6c19a640d48815ffe8b405d27837b1c6f
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-03-22 11:38:45 +0000

    add tests for graph callbacks

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -11,7 +11,7 @@ TEST(GraphTests, countNodes) {
     ASSERT_EQ(graph.countNodes(), nodes);
 }
 
-TEST(GraphTests, countEdges) {
+TEST(GraphTests, connect) {
     int nodes = 3*3;
     Graph graph(nodes, nodes*6);
 
@@ -20,7 +20,7 @@ TEST(GraphTests, countEdges) {
     ASSERT_EQ(graph.countEdges(), 1);
 }
 
-TEST(GraphTests, countBidirectedEdges) {
+TEST(GraphTests, bidirectedConnect) {
     int nodes = 3*3;
     Graph graph(nodes, nodes*6);
 
@@ -31,7 +31,7 @@ TEST(GraphTests, countBidirectedEdges) {
     ASSERT_EQ(graph.countEdges(), 6);
 }
 
-TEST(GraphTests, countDirectedEdges) {
+TEST(GraphTests, directedConnect) {
     int nodes = 3*3;
     Graph graph(nodes, nodes*6);
 
@@ -40,6 +40,63 @@ TEST(GraphTests, countDirectedEdges) {
     graph.directedConnect(2, 0);
 
     ASSERT_EQ(graph.countEdges(), 3);
+}
+
+TEST(GraphTests, nodesAreConnected) {
+    int nodes = 3*3;
+    Graph graph(nodes, nodes*6);
+
+    graph.bidirectedConnect(0, 1);
+    graph.directedConnect(1, 2);
+
+    ASSERT_EQ(graph.nodesAreConnected(0, 1), true);
+    ASSERT_EQ(graph.nodesAreConnected(1, 0), true);
+    ASSERT_EQ(graph.nodesAreConnected(1, 2), true);
+    ASSERT_EQ(graph.nodesAreConnected(2, 1), false);
+}
+
+TEST(GraphTests, forEachEdgeFrom)
+{
+    std::vector<Edge> calledEdges;
+
+    auto callback = [&calledEdges](const Edge edge) {
+        calledEdges.push_back(edge);
+    };
+
+    int nodes = 3*3;
+    Graph graph(nodes, nodes*6);
+
+    graph.directedConnect(0, 1);
+    graph.directedConnect(0, 2);
+    graph.directedConnect(1, 3);
+    graph.forEachEdgeFrom(0, callback);
+
+    std::vector<int> expected = {1, 2};
+    for (int i = 0; i < expected.size(); i++) {
+        ASSERT_EQ(calledEdges[i].to, expected[i]);
+    }
+}
+
+TEST(GraphTests, forEachEdge)
+{
+    std::vector<Edge> calledEdges;
+
+    auto callback = [&calledEdges](const Edge edge) {
+        calledEdges.push_back(edge);
+    };
+
+    int nodes = 3*3;
+    Graph graph(nodes, nodes*6);
+
+    graph.directedConnect(0, 1);
+    graph.directedConnect(0, 2);
+    graph.directedConnect(1, 3);
+    graph.forEachEdge(callback);
+
+    std::vector<int> expected = {1, 2, 3};
+    for (int i = 0; i < expected.size(); i++) {
+        ASSERT_EQ(calledEdges[i].to, expected[i]);
+    }
 }
 
 #endif
