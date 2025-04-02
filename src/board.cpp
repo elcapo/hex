82eef14e449bd6db1f1a9ba9ad363a33bc1c797d
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-04-02 15:48:58 +0100

    remove comments from the board implementation that are already in its header

--- src/board.cpp
+++ src/board.cpp
@@ -2,13 +2,6 @@
 #include <cmath>
 #include "board.hpp"
 
-/**
- * Get a given turn as a single character.
- *
- * @return 'B' for Turn::Blue
- *         'R' for Turn::Red
- *         'o' otherwise
- */
 const char* turnAsChar(const Turn& t)
 {
     switch (t) {
@@ -21,13 +14,6 @@ const char* turnAsChar(const Turn& t)
     }
 }
 
-/**
- * Get a given turn as a printable label.
- *
- * @return 'Blue' for Turn::Blue
- *         'Red' for Turn::Red
- *         'Undecided' for Turn::Undecided
- */
 std::string turnAsLabel(const Turn& t)
 {
     switch (t) {
@@ -69,9 +55,6 @@ Board::Board(int size) :
     }
 };
 
-/**
- * Passes the turn to the next player
- */
 void Board::next()
 {
     if (turn == Turn::Blue) {
@@ -83,12 +66,6 @@ void Board::next()
     movements++;
 }
 
-/**
- * Connect a blue movement with its surrounding blue positions.
- *
- * @param row The row number.
- * @param col The column number.
- */
 void Board::connectBlue(int row, int col)
 {
     if (exists(row - 1, col) && isBlue(row - 1, col))
@@ -110,12 +87,6 @@ void Board::connectBlue(int row, int col)
         blueGraph.connect(cell(row, col), cell(row + 1, col));
 }
 
-/**
- * Connect a red movement with its surrounding red positions.
- *
- * @param row The row number.
- * @param col The column number.
- */
 void Board::connectRed(int row, int col)
 {
     if (exists(row - 1, col) && isRed(row - 1, col))
@@ -137,54 +108,21 @@ void Board::connectRed(int row, int col)
         redGraph.connect(cell(row, col), cell(row + 1, col));
 }
 
-/**
- * Checks if a cell exists by its row and column numbers.
- *
- * Both the row and the column number are expected to be
- * in the range from 0 to size-1.
- *
- * @param row The row number.
- * @param col The column number.
- *
- * @return Whether the cell exists or not.
- */
 bool Board::exists(int row, int col) const
 {
     return row >= 0 && row < size && col >= 0 && col < size;
 }
 
-/**
- * Get the row number of the rendered board that corresponds
- * to a given row and column of the board.
- *
- * @return Row number.
- */
 int Board::getY(int row, int col) const
 {
     return row*2;
 }
 
-/**
- * Get the column number of the rendered board that corresponds
- * to a given row and column of the board.
- *
- * @return Column number.
- */
 int Board::getX(int row, int col) const
 {
     return col*4 + row*2;
 }
 
-/**
- * Obtain the number of a cell counting from its top left
- * cell, in a first left then down order.
- *
- * @param row The row number.
- * @param col The column number.
- *
- * @return A number for each cell in a range from 0 to size-1.
- *         Also can return -1, if the cell does not exist.
- */
 int Board::cell(int row, int col) const
 {
     if (! exists(row, col)) {
@@ -194,34 +132,16 @@ int Board::cell(int row, int col) const
     return row * size + col;
 }
 
-/**
- * Get the color of the current turn.
- *
- * @return Color of the player that should do the next move.
- */
 const Turn& Board::current() const
 {
     return turn;
 }
 
-/**
- * Get the number of movements that have been played.
- *
- * @return Number of movements.
- */
 const int& Board::countMovements() const
 {
     return movements;
 }
 
-/**
- * Check if a position is owned by the blue player.
- *
- * @param row The row number.
- * @param col The column number.
- *
- * @return True if the position is owned by the blue player.
- */
 bool Board::isBlue(int row, int col)
 {
     Position position = std::make_pair(row, col);
@@ -229,14 +149,6 @@ bool Board::isBlue(int row, int col)
     return positions[position] == Turn::Blue;
 }
 
-/**
- * Check if a position is owned by the red player.
- *
- * @param row The row number.
- * @param col The column number.
- *
- * @return True if the position is owned by the red player.
- */
 bool Board::isRed(int row, int col)
 {
     Position position = std::make_pair(row, col);
@@ -244,9 +156,6 @@ bool Board::isRed(int row, int col)
     return positions[position] == Turn::Red;
 }
 
-/**
- * Check if any of the player's already won.
- */
 void Board::checkGame()
 {
     if (blueDijkstra.nodesAreConnected(cell(0, 0), cell(size - 1, size - 1))) {
@@ -260,24 +169,11 @@ void Board::checkGame()
     }
 }
 
-/**
- * Color of the player who won the game.
- *
- * @return Color of the player who won the game.
- *         If the game hasn't finished yet, the
- *         value "undecided" will be returned.
- */
 Turn Board::playerWon()
 {
     return winner;
 }
 
-/**
- * Set the given cell with the color of the current player.
- *
- * @param row The row number.
- * @param col The column number.
- */
 void Board::set(int row, int col)
 {
     if (turn == Turn::Undecided)
@@ -304,24 +200,12 @@ void Board::set(int row, int col)
     next();
 }
 
-/**
- * Get the color of a given cell.
- *
- * @param row The row number.
- * @param col The column number.
- *
- * @return The color of the cell.
- */
 const Turn& Board::get(int row, int col) const
 {
     Position position = std::make_pair(row, col);
     return positions[position];
 }
 
-/**
- * Apply the pie rule after the first movement, changing the color
- * of the first piece and passing the turn.
- */
 void Board::pieRule()
 {
     if (movements != 1) {
@@ -336,32 +220,14 @@ void Board::pieRule()
     next();
 }
 
-/**
- * Get a read only version of the blue graph.
- *
- * @return Read only pointer to the blue graph.
- */
 const Graph& Board::getBlueGraph() const {
     return blueGraph;
 }
 
-/**
- * Get a read only version of the red graph.
- *
- * @return Read only pointer to the red graph.
- */
 const Graph& Board::getRedGraph() const {
     return redGraph;
 }
 
-/**
- * Facilitate iterating over the lines of the board
- * in order to print them or redirect them to the
- * standard output.
- *
- * @param callback Function that will be called back
- *        with the corresponding line.
- */
 void Board::forEachLine(std::function<void(const char* line)> callback) const
 {
     char line[128] = "";
@@ -418,14 +284,6 @@ void Board::forEachLine(std::function<void(const char* line)> callback) const
     }
 }
 
-/**
- * Facilitate iterating over the pieces of the board
- * in order to print them or redirect them to the
- * standard output.
- *
- * @param callback Function that will be called back
- *        with the corresponding piece.
- */
 void Board::forEachPiece(std::function<void(const int row, const int col, Turn turn)> callback) const
 {
     Position position;
@@ -439,10 +297,6 @@ void Board::forEachPiece(std::function<void(const int row, const int col, Turn t
     }
 }
 
-/**
- * Override the << operator in order to facilitate streaming the
- * board over a standard output.
- */
 std::ostream& operator<<(std::ostream& os, const Board& board)
 {
     board.forEachLine([&os](const char* line) {
